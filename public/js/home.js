9cddbd6807f509ccb01196ef48b7fbf980831820
Pankaj Sharma <118182606+PankajSharma3@users.noreply.github.com>
2024-06-23 17:35:36 +0530

    Add files via upload

---
+++ public/js/home.js
@@ -0,0 +1,9 @@
+function checkUserDetails() {
+    const user = localStorage.getItem('user');
+    if (user)
+    window.location.href = '/dashboard';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    checkUserDetails();
+});
